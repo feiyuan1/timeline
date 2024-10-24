585f1e14c8ef5bf81098f76d27a71d87c1431c9f
feiyuan peng <feiyuan2987@163.com>
2024-10-24 21:47:33 +0800

    chore: ts 调整

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+export default function initReact() {
+  const root = document.getElementById('root')
+
+  createRoot(root).render(<App />)
+}
