af586e9aa95d924e28cffd52580132351204dcb9
feiyuan peng <feiyuan2987@163.com>
2024-11-03 16:44:41 +0800

    chore: 补充 LineGroupContent 的测试用例

--- __tests__/LineContent.test.tsx
+++ __tests__/LineContent.test.tsx
@@ -114,9 +114,29 @@ describe('test LineGroupContent UI', () => {
   })
 })
 
-// describe('test LineGroupCotnent interaction', () => {
-//   it('click tab', () => {})
-// })
+describe('test LineGroupCotnent interaction', () => {
+  const data = list[0] as LineGroup
+  it('click content', () => {
+    // navigate to /line-group/:id
+    const { container } = renderLineGroup(data)
+    const content = container.querySelector<HTMLDivElement>(
+      '.MuiCardContent-root'
+    )
+    expect(content).toBeInTheDocument()
+    if (!content) {
+      return
+    }
+
+    fireEvent.click(
+      content,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    )
+    expect(window.location.pathname).toBe(`/line-group/${data.id}`)
+  })
+})
 
 // 点击 container 中第 index 个 tab
 const changeTab = (container: HTMLElement, index: number) => {
