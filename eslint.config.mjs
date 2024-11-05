19e80298b7448769145aec64d6ea7def78b0c36c
feiyuan peng <feiyuan2987@163.com>
2024-11-05 16:20:43 +0800

    chore: eslint react jsx-runtime

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
-// import pluginReact from 'eslint-plugin-react'
+import pluginReact from 'eslint-plugin-react'
 import PluginPrettier from 'eslint-plugin-prettier/recommended'
 
 export default [
@@ -9,7 +9,7 @@ export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  // pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
   {
     rules: {
       '@typescript-eslint/no-require-imports': 'off',
