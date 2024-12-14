9a262c5505f775e648cbb2ee521e1c33bd54a0e5
feiyuan peng <feiyuan2987@163.com>
2024-12-14 16:39:09 +0800

    chore: backend 增加 linegroup 接口

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,7 +1,9 @@
-const routeLine = require('./line')
 import Router = require('koa-router')
+const routeLine = require('./line')
+const routeGroup = require('./lineGroup')
 
 const routes = (router: Router) => {
   routeLine(router)
+  routeGroup(router)
 }
 module.exports = routes
