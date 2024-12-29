6b8ae9c357741f7de6bb14d0b02aa19b12428a1f
feiyuan peng <feiyuan2987@163.com>
2024-12-29 17:51:53 +0800

    feat: node page support add&delete&list-show&useLoading and add component List

---
+++ src/components/List.tsx
@@ -0,0 +1,72 @@
+import { ReactNode } from 'react'
+import { useNavigate } from 'react-router-dom'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListSubheader from '@mui/material/ListSubheader'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import { getLink } from 'utils/index'
+import { Contents } from 'types'
+
+export interface Item<T> {
+  (props: { index: number; data: T }): ReactNode
+}
+
+const CustomList = <T extends Contents>({
+  list,
+  label,
+  Item
+}: {
+  list: T[]
+  label: string
+  Item: Item<T>
+}) => {
+  return (
+    <List
+      aria-labelledby={label}
+      subheader={<ListSubheader component="p">{label}</ListSubheader>}
+    >
+      {list.map((data, index) => (
+        <Item index={index} data={data} key={index} />
+      ))}
+    </List>
+  )
+}
+
+export interface Content<T extends Contents> {
+  (props: { data: T }): ReactNode
+}
+
+export const customListItem =
+  <T extends Contents>({
+    shouldNavigate = true,
+    Content
+  }: {
+    shouldNavigate?: boolean
+    Content: Content<T>
+  }): Item<T> =>
+  ({ data, index }) => {
+    const navigate = useNavigate()
+    const handleClick = () => {
+      if (shouldNavigate) {
+        navigate(getLink(data))
+      }
+    }
+
+    return (
+      <ListItem key={index}>
+        <Card
+          sx={{ width: '100%', '&:hover': { cursor: 'pointer' } }}
+          onClick={handleClick}
+        >
+          <CardHeader title={data.name} />
+          <CardContent>
+            <Content data={data} />
+          </CardContent>
+        </Card>
+      </ListItem>
+    )
+  }
+
+export default CustomList
--- src/pages/node/Node.tsx
+++ src/pages/node/Node.tsx
@@ -1,9 +1,63 @@
+import { useCallback, useMemo } from 'react'
+import Stack from '@mui/material/Stack'
+import { AddButton } from 'components/CustomButton'
+import FormModal from 'components/FormModal'
+import CustomList, { Content, customListItem, Item } from 'components/List'
+import PageContainer from 'components/PageContainer'
+import DeleteForm from './DeleteForm'
+import { formatDate } from 'utils/date'
+import useLoading from 'utils/useLoading'
 import useRequiredParams from 'utils/useRequiredParams'
+import useToggle from 'utils/useToggle'
+import { logProps as getLogProps } from '_constants/form'
+import { getNode } from 'api/node'
+import { LineNode, Log } from 'types'
 
-const Node = () => {
-  const { id } = useRequiredParams<{ id: string }>()
+const Content: Content<Log> = ({ data }) => (
+  <Stack
+    dangerouslySetInnerHTML={{ __html: data.content }}
+    component="p"
+    sx={{ whiteSpace: 'pre-wrap' }}
+  />
+)
 
-  return <>{id}</>
+const Item: Item<Log> = customListItem({ Content })
+
+const Node = ({
+  data: { id, name, logs, key: originalKey },
+  data
+}: {
+  data: LineNode
+}) => {
+  const [open, toggle] = useToggle()
+  const logProps = useMemo(() => getLogProps({ nodeId: id }), [id])
+  const key = useMemo(() => {
+    if (typeof originalKey === 'string') {
+      return originalKey
+    }
+    return formatDate(originalKey)
+  }, [originalKey])
+
+  return (
+    <>
+      key:{key}
+      <CustomList<Log> Item={Item} list={logs} label={name} />
+      <AddButton onClick={toggle} />
+      <FormModal open={open} handleClose={toggle} {...logProps} />
+      <DeleteForm data={data} />
+    </>
+  )
 }
 
-export default Node
+const NodePage = () => {
+  const { id } = useRequiredParams<{ id: string }>()
+  const initData = useCallback(
+    () => getNode({ id }).then((data) => data[0]),
+    [id]
+  )
+  const { elem } = useLoading<LineNode>(initData, { Component: Node })
+
+  return <PageContainer>{elem}</PageContainer>
+}
+
+export default NodePage
