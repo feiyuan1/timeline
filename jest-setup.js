5252da2e3a76d79b112b268d1fc9f8ee8116df6f
feiyuan peng <feiyuan2987@163.com>
2025-01-05 12:02:21 +0800

    feat: test support fetch mock&css mock

--- jest-setup.js
+++ jest-setup.js
@@ -1,3 +1,22 @@
 require('@testing-library/jest-dom')
+const fetchMock = require('fetch-mock').default
+/**
+ * polyfill for fetchMock
+ */
+require('whatwg-fetch')
+const { ReadableStream } = require('web-streams-polyfill')
+
+if (!globalThis.ReadableStream) {
+  globalThis.ReadableStream = ReadableStream
+}
+
+fetchMock.mockGlobal()
+
+fetchMock.config = Object.assign(fetchMock.config, {
+  Request,
+  Response,
+  Headers,
+  fetch
+})
 
 module.exports = async function () {}
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@
 
 module.exports = {
   testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: ['node'],
+    url: 'http://locahost:3000'
+  },
   transform: {
     '^.+.(tsx|ts)?$': ['ts-jest', {}]
   },
@@ -17,10 +21,12 @@ module.exports = {
     '^assets(.*)$': '<rootDir>/src/assets$1',
     '^utils(.*)$': '<rootDir>/src/utils$1',
     '^types(.*)$': '<rootDir>/src/types$1',
-    '^_constants': '<rootDir>/src/constants',
+    '^_constants(.*)$': '<rootDir>/src/constants$1',
+    '^public(.*)$': '<rootDir>/src/public$1',
+    '^api(.*)$': '<rootDir>/src/api$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__tests__/__mocks__/fileMock.js'
-    // '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js'
+      '<rootDir>/__tests__/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__tests__/__mocks__/styleMock.js'
   },
   testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)']
 }
