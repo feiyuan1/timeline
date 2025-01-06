59faea13da8c19dbd5a21ee044cd0f50cc1dfca7
feiyuan peng <feiyuan2987@163.com>
2025-01-06 16:09:10 +0800

    feat: jest with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 import pluginReactHook from 'eslint-plugin-react-hooks'
 import PluginPrettier from 'eslint-plugin-prettier/recommended'
+import pluginJest from 'eslint-plugin-jest'
 
 export default [
   {
@@ -37,5 +38,19 @@ export default [
       ]
     }
   },
-  PluginPrettier
+  PluginPrettier,
+  {
+    files: ['__tests__/**/*.test.(ts|js|tsx)', 'jest-setup.js'],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error'
+    }
+  }
 ]
