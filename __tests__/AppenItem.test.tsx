acd7b730f399a04408c8b5cd7576d3925017604c
feiyuan peng <feiyuan2987@163.com>
2025-01-06 17:54:16 +0800

    fix: 修改原有 AppendItem case 的实现

--- __tests__/AppenItem.test.tsx
+++ __tests__/AppenItem.test.tsx
@@ -1,17 +1,17 @@
-import { fireEvent, render, screen, within } from '@testing-library/react'
+import {
+  fireEvent,
+  getAllByRole,
+  getByText,
+  render,
+  screen
+} from '@testing-library/react'
 import AppendItem from 'pages/main/AppendItem'
 
 const showMenu = (container: HTMLElement) => {
-  // TODO getByRole 会引发 type="submit" button 某个事件，导致表单触发提交验证
+  // TODO 因为存在 test case: AddButton is visible in initial state => 这里还需要再次校验吗？
   const button = container.querySelector<HTMLButtonElement>(
     '.MuiIconButton-root'
-  )
-  expect(button).toBeInTheDocument()
-  if (!button) {
-    return
-  }
-  const addIcon = screen.getByTestId('AddIcon')
-  expect(button).toContainElement(addIcon)
+  )!
   fireEvent.click(
     button,
     new MouseEvent('click', {
@@ -19,27 +19,33 @@ const showMenu = (container: HTMLElement) => {
       cancelable: true
     })
   )
-  const menu = document.body.querySelector<HTMLDivElement>('.MuiMenu-root')
-  return menu
+  // TODO 因为存在 test case：click IconButton and Menu should be shouwn => 这里还需要再次校验吗？
+  return screen.getByRole('presentation')
 }
 
 describe('AppendItem UI', () => {
-  it('AddIcon is visible', () => {
-    render(<AppendItem />)
-    const addIcon = screen.getByTestId('AddIcon')
-    expect(addIcon).toBeVisible()
-  })
-
-  it('Menu', () => {
+  // TODO getByRole 会引发 type="submit" button 某个事件，导致表单触发提交验证
+  it('AddButton is visible in initial state', () => {
     const { container } = render(<AppendItem />)
-    const menu = showMenu(container)
-    if (!menu) {
+    const button = container.querySelector<HTMLButtonElement>(
+      '.MuiIconButton-root'
+    )
+    expect(button).toBeInTheDocument()
+    if (!button) {
       return
     }
-    const menuList = within(menu).getAllByRole('menuitem')
+    const addIcon = screen.getByTestId('AddIcon')
+    expect(addIcon).toBeVisible()
+    expect(button).toContainElement(addIcon)
+  })
+
+  it('Menu UI', () => {
+    const { container } = render(<AppendItem />)
+    const menu = showMenu(container)!
+    const menuList = getAllByRole(menu, 'menuitem')
     expect(menuList.length).toBe(2)
-    expect(within(menu).getByText('线路')).toBeInTheDocument()
-    expect(within(menu).getByText('线路组')).toBeInTheDocument()
+    expect(getByText(menu, '线路')).toBeInTheDocument()
+    expect(getByText(menu, '线路组')).toBeInTheDocument()
   })
   it('LineModal', () => {})
   it('LineGroupModal', () => {})
@@ -49,12 +55,9 @@ describe('AppendItem interaction', () => {
   it('click IconButton and Menu should be shouwn', () => {
     const { container } = render(<AppendItem />)
     const menu = showMenu(container)
-    if (!menu) {
-      return
-    }
-    expect(menu).toHaveAttribute('role', 'presentation')
-    expect(menu).toBeVisible()
+    expect(menu).toHaveClass('MuiMenu-root')
   })
+
   it('click add line button in menu', () => {
     //  Menu should be closed; LineModal is visible
   })
@@ -66,9 +69,10 @@ describe('AppendItem interaction', () => {
 describe('AppendItem logic', () => {
   it('Menu is hidden by default', () => {
     render(<AppendItem />)
-    const menu = document.body.querySelector<HTMLDivElement>('.MuiMenu-root')
+    const menu = screen.queryByRole('presentation')
     expect(menu).toBe(null)
   })
+
   it('LineModal is hidden by default', () => {})
   it('LineGroupModal is hidden by default', () => {})
 })
