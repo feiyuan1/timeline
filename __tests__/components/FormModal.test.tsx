4a9200c780717abe67c724a309968888e86d3e6e
feiyuan peng <feiyuan2987@163.com>
2025-01-09 09:20:17 +0800

    fix: get input wrapped in act in FormModal test case

--- __tests__/components/FormModal.test.tsx
+++ __tests__/components/FormModal.test.tsx
@@ -24,11 +24,27 @@ const TestFormModal = () => (
   </FormModal>
 )
 
+const getInput = async (name: string): Promise<HTMLInputElement> => {
+  // 被 FormControl（from mui）包裹的表单控件会在获取时触发校验 => invalid event handler 中包含更新状态的逻辑，需要包裹在 act 中
+  const input: { current: HTMLInputElement | null } = { current: null }
+  await act(
+    async () =>
+      (input.current = screen.getByRole<HTMLInputElement>('textbox', {
+        name
+      }))
+  )
+
+  return input.current!
+}
+
 describe('FormModal UI', () => {
   it('title correct', async () => {
     render(<TestFormModal />)
-    const title = screen.getByRole('heading', { level: 2 })
-    expect(title).toHaveAttribute('id', 'form-modal-title')
+    const titleRef: { current: HTMLElement | null } = { current: null }
+    await act(
+      async () => (titleRef.current = screen.getByRole('heading', { level: 2 }))
+    )
+    expect(titleRef.current).toHaveAttribute('id', 'form-modal-title')
   })
 })
 
@@ -36,9 +52,7 @@ describe('FormModal interaction', () => {
   it('click submit button to call submit api', async () => {
     render(<TestFormModal />)
     const modal = screen.getByRole('presentation')
-    const nameInput = screen.getByRole('textbox', {
-      name: 'name'
-    })
+    const nameInput = await getInput('name')
     fireEvent.input(nameInput, { target: { value: formData.name } })
     const form = modal.querySelector('form')!
     await act(async () => fireEvent.submit(form))
@@ -52,9 +66,7 @@ describe('FormModal logic', () => {
   it('click submit button and it should be disabled until submit handler finished', async () => {
     render(<TestFormModal />)
     const modal = screen.getByRole('presentation')
-    const nameInput = screen.getByRole('textbox', {
-      name: 'name'
-    })
+    const nameInput = await getInput('name')
     fireEvent.input(nameInput, { target: { value: formData.name } })
     const form = modal.querySelector('form')!
     // TODO should use a better way to get submit button
@@ -62,6 +74,6 @@ describe('FormModal logic', () => {
     expect(button).not.toBeDisabled()
     fireEvent.submit(form)
     expect(button).toBeDisabled()
-    await waitFor(() => expect(button).not.toBeDisabled())
+    await waitFor(async () => expect(button).not.toBeDisabled())
   })
 })
