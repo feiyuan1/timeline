fe0e5e17ac80814e2a7f73bb8ebf7f00fe3dc623
feiyuan peng <feiyuan2987@163.com>
2025-01-09 09:33:06 +0800

    chore: use getInput util in Formmodal

--- __tests__/components/FormModal.test.tsx
+++ __tests__/components/FormModal.test.tsx
@@ -1,6 +1,7 @@
 import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
 import FormModal from 'components/FormModal'
 import { TextField } from 'components/RegularInput'
+import { getInput } from '../utils'
 
 const formProps = {
   title: 'title',
@@ -23,19 +24,6 @@ const TestFormModal = () => (
     <TextField name="desc" label="desc" />
   </FormModal>
 )
-
-const getInput = async (name: string): Promise<HTMLInputElement> => {
-  // 被 FormControl（from mui）包裹的表单控件会在获取时触发校验 => invalid event handler 中包含更新状态的逻辑，需要包裹在 act 中
-  const input: { current: HTMLInputElement | null } = { current: null }
-  await act(
-    async () =>
-      (input.current = screen.getByRole<HTMLInputElement>('textbox', {
-        name
-      }))
-  )
-
-  return input.current!
-}
 
 describe('FormModal UI', () => {
   it('title correct', async () => {
