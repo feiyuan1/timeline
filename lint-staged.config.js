440d280f92fb9a152877c0ec1b68f94afdf7f5d0
feiyuan2987 <feiyuan2987@163.com>
2025-01-19 17:12:28 +0800

    fix: wrap test in lint-staged flow

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,5 @@
 module.exports = {
+  '__tests__/**/*.test.{ts,js,tsx}': (stagedFiles) =>
+    `npm test ${stagedFiles.join(' ')}`,
   '!.husky/**/**': (stagedFiles) => `npx eslint --fix ${stagedFiles.join(' ')}`
 }
